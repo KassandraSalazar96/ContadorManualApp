07f4de183fa95919638a3829138202b995203644
Kassandra Salazar <14470225@itcampeche.edu.mx>
2017-10-09 13:50:56 -0500

    ContadorApp: Se agregó el manejo de eventos a la interfaz de ususuario gráfica

--- app/src/main/java/contador/kassandrasalazar/android/contador/MainActivity.java
+++ app/src/main/java/contador/kassandrasalazar/android/contador/MainActivity.java
@@ -2,16 +2,42 @@ package contador.kassandrasalazar.android.contador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText mCampoConteo;
+    Button mBotonContar;
+    Button mBotonReiniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCampoConteo = (EditText) findViewById(R.id.etiqueta_conteo);
+        mBotonContar = (Button) findViewById(R.id.boton_contar);
+        mBotonReiniciar = (Button) findViewById(R.id.boton_reiniciar);
+
+        mBotonContar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String m = mCampoConteo.getText().toString();
+                int contar = Integer.parseInt(m);
+                contar++;
+                String resultadoConteo = Integer.toString(contar);
+                mCampoConteo.setText(resultadoConteo);
+            }
+        });
+        mBotonReiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCampoConteo.setText("0");
+            }
+        });
+
 
 
     }
